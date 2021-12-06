330cd1a73435bd6d641e15992c1bdfca8fdbf229
Phillip Schmidt <pjschmidt3@outlook.com>
2021-12-06 15:39:04 -0600

    minor

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
-console.log("Hello World!\n==========\n");
+class Person {
+  constructor (name, pets, residence, hobbies) {
+    this.name = name;
+    this.pets = pets;
+    this.residence = residence;
+    this.hobbies = hobbies;
+  }
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+  info () {
+    console.log('Name: ', this.name);
+    console.log('Pets: ',  this.pets);
+    console.log('Residence: ', this.residence);
+    console.log('Hobbies', this.hobbies);
+  }
+
+  soundOff () {
+
+  }
+
+  greeting () {
+    console.log('Hello fellow human');
+  }
+}
+
+class Coder extends Person {
+  constructor (name, pets, residence, hobbies) {
+    super(name, pets, residence, hobbies);
+    this.occupation = "Full Stack Web Developer";
+  }
+
+  greeting () {
+    console.log("Yo I'm a coder");
+  }
+}
+
+const coder = new Coder("Phil", ["dog", "cat"], "Louisiana", ["programming"]);
+
+console.log(coder.name);
+console.log(coder.pets);
+console.log(coder.residence);
+console.log(coder.hobbies);
+coder.info();
+coder.greeting();
